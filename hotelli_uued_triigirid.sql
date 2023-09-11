d7c2ec4139d790484059cb43f25ad7cdc1646196
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-09-11 11:16:48 +0300

    hotelli_uued_triigirid.sql

--- hotelli_uued_triigirid.sql
+++ hotelli_uued_triigirid.sql
@@ -14,3 +14,33 @@ INSERT INTO reserved_room(number_of_rooms, room_type_id, reservation_id,status)
 VALUES('235','1','1','ready');
 SELECT* from reserved_room
 SELECT * from logi
+--kontroll
+INSERT INTO reserved_room(number_of_rooms, room_type_id, reservation_id,status)
+VALUES('235','1','1','ready');
+SELECT* from reserved_room
+SELECT * from logi
+
+
+  
+
+CREATE TRIGGER hotelli_kuendamine
+ON reserved_room
+FOR UPDATE
+AS
+INSERT INTO logi(kuupaev, andmed, kasutaja)
+SELECT GETDATE(),
+CONCAT(' Vanad andmed ', deleted.number_of_rooms,', ', r1.room_type_id,', ',re1.reservation_id,
+' Uued - ', inserted.number_of_rooms, ', ', r.room_type_id,', ',re.reservation_id),
+USER
+FROM deleted
+inner join inserted ON deleted.reserved_room_id=inserted.reserved_room_id
+INNER JOIN room_type r ON inserted.room_type_id=r.room_type_id
+INNER JOIN reservation re ON inserted.reservation_id=re.reservation_id
+INNER JOIN room_type r1 ON deleted.room_type_id=r.room_type_id
+INNER JOIN reservation re1 ON deleted.reservation_id=re.reservation_id
+--kontroll
+SELECT * FROM reserved_room
+UPDATE reserved_room SET room_type_id=2, reservation_id=2
+WHERE number_of_rooms='235'
+SELECT * FROM reserved_room
+SELECT * FROM logi

71e200d084624e2b0f42ff4c3b47d619891d139a
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-09-27 13:18:45 +0300

    Update Indeksid.sql

--- Indeksid.sql
+++ Indeksid.sql
@@ -29,22 +29,22 @@ EXECUTE SP_HELPCONSTRAINT tblEmployee
 CREATE UNIQUE INDEX IX_tblEmployee_City
 ON tblEmployee(City)
 WITH IGNORE_DUP_KEY
-
+-- создаёт уникальный индекс за счёт столбца City в таблице tblEmployee с IGNORE_DUP_KEY.
 
 ---------------------------------- 38. Indeksi plussid ja miinused
 
 Create NonClustered Index IX_tblEmployee_Salary
 On tblEmployee(Salary ASC)
-
+-- создаёт некластеризованный  индекс за счёт столбца Salary в таблице tblEmployee по упорядку ASC
 
 SELECT * FROM tblEmployee WHERE Salary > 4000 and Salary < 8000
-
+-- выводит всю таблицу с учётом на тот порядок(Salary > 4000 and Salary < 8000) который был сделан
 
 SELECT * FROM tblEmployee;
 DELETE FROM tblEmployee WHERE Salary = 2500;
 UPDATE tblEmployee SET Salary = 9000 WHERE Salary = 7500;
 SELECT * FROM tblEmployee;
-
+-- показывает таблицу tblEmployee, затем удаляет запись из этой же таблицы где Salary = 2500, потом обновляет Salary на 9000 где 7500 было, и в конце опять показывает уже изменённую таблицу tblEmployee
 
 SELECT * FROM tblEmployee order by Salary;
 
